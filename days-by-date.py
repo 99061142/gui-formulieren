a9a68ba0e8f0b64ec9dd12a631f7af3d8b0a8cb3
99061142 <99061142@mydavinci.nl>
2021-12-20 22:36:04 +0100

    Show the user what the day difference is
    * Notes
    
    The year input can't get higher than 4 numbers.

--- days-by-date.py
+++ days-by-date.py
@@ -1,19 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
-from calendar import calendar
 from calendar import monthrange
 from calendar import month_name
 from datetime import datetime
+from datetime import date
 
 
 # Makes the window
 window = tk.Tk()
-window.geometry('300x200') 
+window.geometry('300x150') 
 window.title('Date by days calculator') # Window title
 
 
-title = tk.Label(text="Date:", font=('arial', 18, 'bold')).pack(fill='y', pady=2) # Title
+title = tk.Label(text="Date:", font=('arial', 18, 'bold')).grid(row=0, columnspan=3) # Add the input to the screen
 
 
 selected_day = tk.IntVar()
@@ -24,27 +25,61 @@ total_days = tk.IntVar()
 
 def make_day_input():
     # create the input for the days
-    day_input = ttk.Combobox(window, textvariable=selected_day)
+    day_input = ttk.Combobox(window, textvariable=selected_day, width=8)
     day_input['values'] = [[day] for day in range(1, total_days.get())] # Add day numbers to the input (first 3 letters)
     day_input['state'] = 'readonly' # User must choose a value that is a valid option
 
-    day_input.pack(fill=tk.X, padx=5, pady=5) # Add the input to the screen
-
+    day_input.grid(row=1, column=0, sticky=tk.E, padx=5, pady=5) # Add the input to the screen
 
 def make_month_input():    
     # create the input for the months
-    month_input = ttk.Combobox(window, textvariable=selected_month)
+    month_input = ttk.Combobox(window, textvariable=selected_month, width=8)
     month_input['values'] = [month_name[month][0:3] for month in range(1, 13)] # Add month names to the input (first 3 letters)
     month_input['state'] = 'readonly' # User must choose a value that is a valid option
 
-    month_input.pack(fill=tk.X, padx=5, pady=5) # Add the input to the screen
+    month_input.grid(row=1, column=1, sticky=tk.E, padx=5, pady=5) # Add the input to the screen
 
 
 def make_year_input():
     # create the input for the year
-    year_input = ttk.Entry(window, textvariable=selected_year)
+    year_input = ttk.Entry(window, textvariable=selected_year, width=8)
 
-    year_input.pack(fill=tk.X, padx=5, pady=5) # Add the input to the screen
+    year_input.grid(row=1, column=2, sticky=tk.E, padx=5, pady=5) # Add the input to the screen
+
+
+def submit_button():
+    # Button to submit the time
+    start_button = tk.Button(
+        window, 
+        font=("arial", 10), 
+        text='go',
+        command=calculate_time
+    )
+
+    start_button.grid(row=2, columnspan=3, pady=20)
+
+
+def calculate_time():
+    future_day = selected_day.get() # Day the user chose
+    future_month = datetime.strptime(selected_month.get(), '%b').month # Month the user chose (as a number)
+    future_year = selected_year.get() # Year the user chose
+
+
+    current_date = date.today() # Local date
+    future_date = date(future_year, future_month, future_day) # Date the user chose
+    difference = future_date - current_date # Date difference
+
+    day_difference = difference.days # Day difference between today's date and the future date
+
+    # Text to show the day difference to the user
+    if day_difference == 0:
+        message = "Dit is vandaag"
+    elif day_difference < 0:
+        message = f"Dit is {day_difference} dagen geleden"
+    else:
+        message = f"Dit is {day_difference} dagen in de toekomst"
+
+    messagebox.showinfo(None, message) # Alertbox to show the day difference to the user
 
 
 def add_local_time():
@@ -73,6 +108,7 @@ def main():
     make_day_input() # Make the input for the days
     make_month_input() # Make the input for the months
     make_year_input() # Make the input for the year
+    submit_button()
 
 
 
