02d3953d68af88e3c7b75e9316c0f3990bec4c61
99061142 <99061142@mydavinci.nl>
2021-12-22 23:19:59 +0100

    Made the starting functions

---
+++ registration-form.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+# Makes the window
+window = tk.Tk()
+window.title("Form")
+
+
+
+# Validate the input of the user
+def validate():
+    pass
+
+
+# Make the inputs for the questions
+def question_inputs():
+    pass
+
+
+# Screen with the questions (That don't need to be true)
+def question_screen():
+    pass
+
+
+# Screen with the important questions (That must be true)
+def head_question_screen():
+    pass
+
+
+
+# Homescreen when the user starts the program
+def homescreen():
+    pass
+
+
+
+
+# When the program starts
+if __name__ == "__main__":
+    homescreen()
+    window.mainloop()
